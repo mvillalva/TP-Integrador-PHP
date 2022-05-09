3a2199f8d7e836dd1cb7c2aa16036d06dc9e118f
mvillalva <maxi.villalva@gmail.com>
2022-05-09 16:08:57 -0300

    TP con JS

--- js/main.js
+++ js/main.js
@@ -13,3 +13,37 @@ const changeLinkState = () => {
 changeLinkState();
 
 window.addEventListener('scroll', changeLinkState);
+
+document.getElementById('cantidad').addEventListener('change', e => {
+  if(e.target.value < 1) {
+    e.target.value = 1
+  }
+})
+
+const form_ticket = document.getElementById('form_ticket')
+
+form_ticket.addEventListener('submit', e => {
+  e.preventDefault()
+
+  const cantidad = document.getElementById('cantidad').value
+  const categoria = document.getElementById('categoria').value
+  let precio = 200 * cantidad
+  let total = 0.00
+
+  if (categoria == 'E') {
+    total = precio - (precio * 0.8)
+  } else if (categoria == 'T') {
+    total = precio - (precio * 0.5)
+  } else if (categoria == 'J') {
+    total = precio - (precio * 0.15)
+  } else {
+    total = precio
+  }
+
+  document.getElementById('total').innerHTML = 'Total a Pagar: $' + total
+
+})
+
+form_ticket.addEventListener('reset', e => { 
+  document.getElementById('total').innerHTML = 'Total a Pagar: $' 
+})
