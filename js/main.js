f1dfd0ace9dc3eded9819da72b7c91f6d619a2da
mvillalva <maxi.villalva@gmail.com>
2022-05-10 11:21:39 -0300

    TP con JS

--- js/main.js
+++ js/main.js
@@ -14,36 +14,37 @@ changeLinkState();
 
 window.addEventListener('scroll', changeLinkState);
 
-document.getElementById('cantidad').addEventListener('change', e => {
-  if(e.target.value < 1) {
-    e.target.value = 1
-  }
-})
 
+/***** Calculo de presion al presionar Resumen *****/
 const form_ticket = document.getElementById('form_ticket')
+
+const calcularPrecio = () => {
+  const cantidad = document.getElementById('cantidad').value
+  const categoria = document.getElementById('categoria').value
+  let precio = 200 * cantidad  
+
+  if (categoria == 'E') {
+    precio -= precio * 0.8
+  } else if (categoria == 'T') {
+    precio -= precio * 0.5
+  } else if (categoria == 'J') {
+    precio -= precio * 0.15
+  }
+
+  document.getElementById('total').innerHTML = precio
+}
 
 form_ticket.addEventListener('submit', e => {
   e.preventDefault()
-
-  const cantidad = document.getElementById('cantidad').value
-  const categoria = document.getElementById('categoria').value
-  let precio = 200 * cantidad
-  let total = 0.00
-
-  if (categoria == 'E') {
-    total = precio - (precio * 0.8)
-  } else if (categoria == 'T') {
-    total = precio - (precio * 0.5)
-  } else if (categoria == 'J') {
-    total = precio - (precio * 0.15)
-  } else {
-    total = precio
-  }
-
-  document.getElementById('total').innerHTML = total
-
+  calcularPrecio()
 })
 
 form_ticket.addEventListener('reset', e => { 
   document.getElementById('total').innerHTML = '' 
+})
+
+document.getElementById('cantidad').addEventListener('change', e => {
+  if(e.target.value < 1) {
+    e.target.value = 1
+  }
 })
